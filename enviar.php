80d82e98c1cb3bf87bd5aa0f4b7ee0399e63f770
juanvprada <juanvprada@gmail.com>
2024-07-07 20:13:16 +0200

    PROYECTO TERMINADO con alert al dar a enviar

--- enviar.php
+++ enviar.php
@@ -2,25 +2,31 @@
 $remitente = $_POST['email'];
 $destinatario = 'pdasfal@hotmail.com';
 $asunto = 'Asunto de ejemplo';
-if (!$_POST){
 
-?>
-
-<?php
-}else{
-    $cuerpo = "Nombre" . $_POST["nombre"] . "\r\n";
-    $cuerpo = "Email" . $_POST["email"] . "\r\n";
-    $cuerpo = "Mensaje" . $_POST["mensaje"] . "\r\n";
+if (!$_POST) {
+    // Manejar el caso en que no hay datos POST, podría ser redirigir o mostrar un error
+    echo "No se recibieron datos.";
+} else {
+    $cuerpo = "Nombre: " . $_POST["nombre"] . "\r\n";
+    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+    $cuerpo .= "Mensaje: " . $_POST["mensaje"] . "\r\n";
     
-$headers = "MIME-Version: 1.0\n";
-$headers = "Content-type: text\plain; charset=utf-8\n";
-$headers = "X-Priority: 3\n";
-$headers = "X-MSMail-Priority: Normal\n";
-$headers = "X-Mailer: php\n";
-$headers = "From: \"".$_POST['nombre']. "\" <".$remitente.">\n";
+    $headers = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "From: \"" . $_POST['nombre'] . "\" <" . $remitente . ">\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
 
-mail($destinatario, $asunto, $cuerpo, $headers);
+    if (mail($destinatario, $asunto, $cuerpo, $headers)) {
+        // Redirigir después de enviar el correo
+        header("Location: index.html");
+        exit();
+    } else {
+        echo "Error al enviar el mensaje.";
+    }
 
-include 'alertForm.html';
+    // Incluir el archivo de alerta si es necesario
+    include 'alertForm.html';
 }
 ?>
---
+++ main.js
@@ -0,0 +1,33 @@
+
+document.getElementById('enviar').onclick = function(){
+  alert('Muchas gracias por escribir!, a la mayor brevedad posible nos pondremos en contacto contigo');
+}
+
+
+
+
+
+
+
+
+
+/* ESTO SOLO ES UNA PRUEBA USANDO ANIME.JS
+
+const layout_cabecera_logo_titulo = document.querySelector('.layout_cabecera_logo_titulo');
+
+const mouseHoverAnimation = () => {
+  anime({
+    targets: '.layout_cabecera_logo_titulo',
+    width:'100%',
+    scale:  {
+      delay: 800,
+      values: 1.5
+    },
+    duration: 1500
+
+  })
+}
+
+
+
+layout_cabecera_logo_titulo.addEventListener('mouseover', mouseHoverAnimation) */
